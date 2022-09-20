3acfd0ee70151e65c8ee04b0fac715c1de96e21e
Chef <kvu6111@gmail.com>
2022-09-19 18:08:31 -0600

    inital file structure and almost finished schema

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee {
+    id: INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name: VARCHAR(30) NOT NULL,
+    last_name: VARCHAR(30) NOT NULL,
+    role_id: INT NOT NULL AUTO_INCREMENT,
+    manager_id: INT NOT NULL AUTO_INCREMENT,
+
+    -- TODO: REFERENCE THE KEYS WHEN THEYRE REFERENCABLE
+    -- FOREIGN KEY (role_id) REFERENCES 
+    -- FOREIGN KEY(manager_id) REFERENCES
+}
